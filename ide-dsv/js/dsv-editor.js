03ad62a8b59ac38eae1d3aa755a85c384dea02b9
StanZGenchev <stanislav.genchev@sap.com>
2021-07-05 16:57:51 +0300

    Initial commit
    Signed-off-by: StanZGenchev <stanislav.genchev@sap.com>

---
+++ ide-dsv/js/dsv-editor.js
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2010-2021 SAP SE or an SAP affiliate company and Eclipse Dirigible contributors
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-FileCopyrightText: 2010-2021 SAP SE or an SAP affiliate company and Eclipse Dirigible contributors
+ * SPDX-License-Identifier: EPL-2.0
+ */
+var dsvView = angular.module('dsv-editor', []);
+
+// Initialize messageHub
+dsvView.factory('$messageHub', [function () {
+    var messageHub = new FramesMessageHub();
+    var message = function (evtName, data) {
+        messageHub.post({ data: data }, evtName);
+    };
+    var on = function (topic, callback) {
+        messageHub.subscribe(callback, topic);
+    };
+    return {
+        message: message,
+        on: on
+    };
+}]);
+
+// Initialize controller
+dsvView.controller('DsvViewController', ['$scope', function ($scope) {
+
+}]);
