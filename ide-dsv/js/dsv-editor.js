2b652cc02ecbddc0a71f1acac75fcf29fd14d2f6
StanZGenchev <stanislav.genchev@sap.com>
2021-07-13 21:02:08 +0300

    Implemented a lot of editing functionality

--- ide-dsv/js/dsv-editor.js
+++ ide-dsv/js/dsv-editor.js
@@ -11,16 +11,20 @@
  */
 
 agGrid.initialiseAgGridWithAngular1(angular);
-let dsvView = angular.module('dsv-editor', ["agGrid", "directive.contextMenu"]);
+let dsvView = angular.module('dsv-editor', ["agGrid"]);
 
 dsvView.controller('DsvViewController', ['$scope', '$window', function ($scope, $window) {
     let messageHub = new FramesMessageHub();
     let contents;
     let csrfToken;
     let manual = false;
-    let csvRaw;
     let isMac = false;
     let isFileChanged = false;
+    $scope.rowMenuStyle = { 'display': 'none' };
+    $scope.headerMenuStyle = { 'display': 'none' };
+    let focusedCell = {};
+    let focusedColumn = undefined;
+    let csvData;
     $scope.dataLoaded = false;
     $scope.ctrlDown = false;
     $scope.ctrlKey = 17;
@@ -65,7 +69,7 @@ dsvView.controller('DsvViewController', ['$scope', '$window', function ($scope, 
         let searchParams = new URLSearchParams(window.location.search);
         $scope.file = searchParams.get('file');
         contents = loadContents($scope.file);
-        csvRaw = contents;
+        csvData = Papa.parse(contents, papaConfig);
         fillGrid();
     }
 
@@ -75,22 +79,37 @@ dsvView.controller('DsvViewController', ['$scope', '$window', function ($scope, 
     }
 
     function fillGrid() {
-        let csvData = Papa.parse(csvRaw, papaConfig);
         let columnDefs = csvData.meta.fields.map(
-            name => (
+            (name, index) => (
                 {
                     headerName: name,
-                    field: name
+                    field: name,
+                    headerComponentParams: {
+                        template:
+                            `<div cid="cid_${index}" class="ag-cell-label-container" role="presentation">` +
+                            '  <span ref="eMenu" class="ag-header-icon ag-header-cell-menu-button"></span>' +
+                            `  <div cid="cid_${index}" ref="eLabel" class="ag-header-cell-label" role="presentation">` +
+                            `    <input id="cid_${index}" class="header-input" type="text">` +
+                            `    <span cid="cid_${index}" id="tid_${index}" ref="eText" class="ag-header-cell-text" role="columnheader"></span>` +
+                            '    <span ref="eSortOrder" class="ag-header-icon ag-sort-order" ></span>' +
+                            '    <span ref="eSortAsc" class="ag-header-icon ag-sort-ascending-icon" ></span>' +
+                            '    <span ref="eSortDesc" class="ag-header-icon ag-sort-descending-icon" ></span>' +
+                            '    <span ref="eSortNone" class="ag-header-icon ag-sort-none-icon" ></span>' +
+                            '    <span ref="eFilter" class="ag-header-icon ag-filter-icon"></span>' +
+                            '  </div>' +
+                            '</div>'
+                    }
                 }
             )
         );
-        columnDefs[0].rowDrag = true; // Adding drag handle to first element only
+        columnDefs[0].rowDrag = true; // Adding drag handle to first column only
+        columnDefs[0].headerCheckboxSelection = true; // Adding checkbox to first column only
         $scope.gridOptions = {
             defaultColDef: {
                 sortable: true,
                 filter: true,
-                editable: true,
                 resizable: true,
+                editable: true,
                 flex: 1
             },
             rowDragManaged: true,
@@ -100,8 +119,6 @@ dsvView.controller('DsvViewController', ['$scope', '$window', function ($scope, 
             columnDefs: columnDefs,
             rowData: csvData.data,
             rowSelection: 'multiple',
-            enableSorting: true,
-            enableColResize: true,
             onColumnResized: function (params) {
                 if (params.finished && manual) {
                     manual = false;
@@ -144,6 +161,82 @@ dsvView.controller('DsvViewController', ['$scope', '$window', function ($scope, 
         }
     }
 
+    function hideContextMenus() {
+        if ($scope.rowMenuStyle.display !== "none") {
+            $scope.rowMenuStyle = {
+                display: "none"
+            };
+        }
+        if ($scope.headerMenuStyle.display !== "none") {
+            $scope.headerMenuStyle = {
+                display: "none"
+            };
+        }
+    }
+
+    function showColumnContextMenu(x, y) {
+        $scope.headerMenuStyle = {
+            position: "fixed",
+            display: "block",
+            left: x + 'px',
+            top: y + 'px'
+        };
+        $scope.rowMenuStyle = {
+            display: "none"
+        };
+    }
+
+    function showRowContextMenu(x, y) {
+        $scope.rowMenuStyle = {
+            position: "fixed",
+            display: "block",
+            left: x + 'px',
+            top: y + 'px'
+        };
+        $scope.headerMenuStyle = {
+            display: "none"
+        };
+    }
+
+    $scope.handleClick = function (event) {
+        if (event.which === 3) {
+            if (
+                event.originalTarget.className.includes("ag-header-cell-label") ||
+                event.originalTarget.className.includes("ag-header-cell-text") ||
+                event.originalTarget.className.includes("ag-cell-label-container")
+            ) {
+                focusedColumn = event.originalTarget.attributes.cid.value;
+                showColumnContextMenu(event.clientX, event.clientY);
+            } else if (event.originalTarget.className.includes("ag-cell")) {
+                focusedCell = $scope.gridOptions.api.getFocusedCell();
+                showRowContextMenu(event.clientX, event.clientY);
+            } else if (
+                !event.originalTarget.className.includes("dropdown-item") &&
+                !event.originalTarget.className.includes("header-input")
+            ) {
+
+                hideContextMenus();
+                hideColumnInput();
+            }
+        } else {
+            try {
+                if (
+                    !event.originalTarget.className.includes("dropdown-item") &&
+                    !event.originalTarget.className.includes("header-input")
+                ) {
+                    hideContextMenus();
+                    hideColumnInput();
+                }
+            } catch (error) {
+                if (error.toString() != 'Error: Permission denied to access property "className"') { // Firefox bug
+                    console.log(error);
+                }
+            }
+        }
+        // let cell = $scope.gridOptions.api.getFocusedCell();
+        // let rows = $scope.gridOptions.api.getSelectedRows();
+    }
+
     $scope.keyDownFunc = function ($event) {
         if ($scope.ctrlDown && String.fromCharCode($event.which).toLowerCase() == 's') {
             $event.preventDefault();
@@ -153,7 +246,7 @@ dsvView.controller('DsvViewController', ['$scope', '$window', function ($scope, 
     };
 
     angular.element($window).bind("keyup", function ($event) {
-        if (isMac && "metaKey" in $event)
+        if (isMac && "metaKey" in $event && $event.metaKey)
             $scope.ctrlDown = false;
         else if ($event.keyCode == $scope.ctrlKey)
             $scope.ctrlDown = false;
@@ -161,7 +254,7 @@ dsvView.controller('DsvViewController', ['$scope', '$window', function ($scope, 
     });
 
     angular.element($window).bind("keydown", function ($event) {
-        if (isMac && "metaKey" in $event)
+        if (isMac && "metaKey" in $event && $event.metaKey)
             $scope.ctrlDown = true;
         else if ($event.keyCode == $scope.ctrlKey)
             $scope.ctrlDown = true;
@@ -173,13 +266,104 @@ dsvView.controller('DsvViewController', ['$scope', '$window', function ($scope, 
     };
 
     $scope.save = function () {
-        contents = $scope.gridOptions.api.getDataAsCsv();;
+        contents = $scope.gridOptions.api.getDataAsCsv();
         saveContents(contents);
     };
 
     $scope.filterCsv = function () {
         $scope.gridOptions.api.setQuickFilter($scope.filterInput);
+    };
+
+    $scope.addRowAbove = function () {
+        let row = {};
+        Object.keys(csvData.data[0]).forEach(key => {
+            row[key] = "";
+        });
+        csvData.data.splice(focusedCell.rowIndex, 0, row);
+        $scope.gridOptions.api.setRowData(csvData.data);
+        hideContextMenus();
+        fileChanged();
+    };
+
+    $scope.addRowBelow = function () {
+        let row = {};
+        Object.keys(csvData.data[0]).forEach(key => {
+            row[key] = "";
+        });
+        csvData.data.splice(focusedCell.rowIndex + 1, 0, row);
+        $scope.gridOptions.api.setRowData(csvData.data);
+        hideContextMenus();
+        fileChanged();
+    };
+
+    $scope.deleteRow = function () {
+        csvData.data.splice(focusedCell.rowIndex, 1);
+        $scope.gridOptions.api.setRowData(csvData.data);
+        hideContextMenus();
+        fileChanged();
+    };
+
+    $scope.addColumn = function () {
+        console.log("edit header");
+        hideContextMenus();
+    };
+
+    $scope.editColumn = function () {
+        hideContextMenus();
+        let columnDefs = $scope.gridOptions.api.getColumnDefs();
+        let index = focusedColumn.replace("cid_", "");
+        columnDefs[index].sortable = false;
+        columnDefs[index].filter = false;
+        $scope.gridOptions.api.setColumnDefs(columnDefs);
+        showColumnInput();
+    };
+
+    function showColumnInput() {
+        let columnInput = $(`#${focusedColumn}`);
+        let columnText = $(`#${focusedColumn.replace("cid", "tid")}`);
+        columnInput.val(columnText.text());
+        columnInput.css({
+            'display': 'inline-block'
+        });
+        columnText.css({
+            'display': 'none'
+        });
+        columnInput.on('keypress', function (e) {
+            if (e.which == 13) {
+                hideColumnInput();
+            }
+        });
     }
+
+    function hideColumnInput() {
+        if (focusedColumn !== undefined && focusedColumn !== null) {
+            let columnInput = $(`#${focusedColumn}`);
+            let newTitle = columnInput.val();
+            let columnText = $(`#${focusedColumn.replace("cid", "tid")}`);
+            columnInput.css({
+                'display': 'none'
+            });
+            columnText.css({
+                'display': 'inline-block'
+            });
+            columnInput.off();
+            if (newTitle != columnText.text()) {
+                let columnDefs = $scope.gridOptions.api.getColumnDefs();
+                let index = focusedColumn.replace("cid_", "");
+                columnDefs[index].sortable = true;
+                columnDefs[index].filter = true;
+                columnDefs[index].headerName = newTitle;
+                $scope.gridOptions.api.setColumnDefs(columnDefs);
+                fileChanged();
+            }
+            focusedColumn = undefined;
+        }
+    };
+
+    $scope.deleteColumn = function () {
+        hideContextMenus();
+        console.log("edit header");
+    };
 
     checkPlatform();
     load();
